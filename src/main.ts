8370a4863404066a41708e43dc08bd34a0ef7099
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2022-12-20 16:01:52 -0300

    FIRST C

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const port = process.env.PORT;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+bootstrap();
