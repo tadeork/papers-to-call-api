872669ce0721539dc74f7451809484103f31f98e
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2022-12-20 17:24:54 -0300

    DB config

---
+++ src/ormconfig.ts
@@ -0,0 +1,10 @@
+export const postgres_config = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
